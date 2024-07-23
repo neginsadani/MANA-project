0e68771d7f7c59a67bce398eeea8b44f1929c475
abbas <bmnswry27@gmail.com>
2024-07-24 00:59:09 +0330

    add js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// script.js
+
+const images = [
+    "./img/ali/img.jpg",
+    "./img/negin/negin.jpg",
+    "./img/home/a1.png",
+    "./img/abbas/code.jpg"
+];
+
+let currentIndex = 0;
+const sliderImage = document.querySelector('.slider-image');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+function updateImage() {
+    sliderImage.src = images[currentIndex];
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+});
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}, 5000);
