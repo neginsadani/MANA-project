471cba62a4b79974d148d1e23bf9d7baaaf93b96
abbas <bmnswry27@gmail.com>
2024-07-24 14:58:24 +0330

    add link js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,34 @@
-const images = [
-    "./img/ali/img.jpg",
-    "./img/negin/negin.jpg",
-    "./img/abbas/abbas-kot.jpg"
+const slides = [
+    { image: "./img/ali/img.jpg", link: "./html/ali.html" },
+    { image: "./img/negin/negin.jpg", link: "./html/negin.html" },
+    { image: "./img/abbas/abbas-kot.jpg", link: "./html/abbas.html" }
 ];
 
 let currentIndex = 0;
 const sliderImage = document.querySelector('.slider-image');
+const sliderLink = document.querySelector('.slider-link');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 
-function updateImage() {
-    sliderImage.src = images[currentIndex];
+function updateSlide() {
+    const currentSlide = slides[currentIndex];
+    sliderImage.src = currentSlide.image;
+    sliderLink.href = currentSlide.link;
 }
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateImage();
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateSlide();
 });
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateImage();
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateSlide();
 });
 
+updateSlide();
+
 setInterval(() => {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateImage();
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateSlide();
 }, 5000);
